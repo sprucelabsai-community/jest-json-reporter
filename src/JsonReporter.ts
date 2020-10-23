8d652c68e6c7553eb4ec06cb0af8806a1e1fab1d
liquidg3 <me@taylorrome.ro>
2020-10-23 09:11:55 -0600

    major: more direct mapping to jest reporter interfaces

--- src/JsonReporter.ts
+++ src/JsonReporter.ts
@@ -1,4 +1,37 @@
-import { Reporter, Test, TestResult } from '@jest/reporters'
+import {
+	AggregatedResult,
+	Context,
+	Reporter,
+	Test,
+	TestResult,
+} from '@jest/reporters'
+
+export declare type Milliseconds = number
+declare type Status =
+	| 'passed'
+	| 'failed'
+	| 'skipped'
+	| 'pending'
+	| 'todo'
+	| 'disabled'
+
+declare type CallSite = {
+	column: number
+	line: number
+}
+
+export declare type AssertionResult = {
+	ancestorTitles: Array<string>
+	duration?: Milliseconds | null
+	failureDetails: Array<unknown>
+	failureMessages: Array<string>
+	fullName: string
+	invocations?: number
+	location?: CallSite | null
+	numPassingAsserts: number
+	status: Status
+	title: string
+}
 
 export const START_DIVIDER =
 	'***************************START_JSON_DIVIDER***************************'
@@ -7,22 +40,47 @@ export const END_DIVIDER =
 	'***************************END_JSON_DIVIDER***************************'
 
 export default class SheetsReporter implements Reporter {
-	public onTestFileResult = undefined
-	public onTestCaseResult = undefined
-	public onTestFileStart = undefined
+	public onTestFileResult(
+		test: Test,
+		testResult: TestResult,
+		aggregatedResult: AggregatedResult
+	) {
+		this.render({
+			status: 'testFileResult',
+			test,
+			testResult,
+			aggregatedResult,
+		})
+	}
 
-	public onRunStart() {}
-	public onRunComplete() {}
-	public getLastError() {}
-	public onTestResult(_: Test, testResult: TestResult) {
+	public onRunStart(results: AggregatedResult) {
+		this.render({ status: 'runStart', results })
+	}
+
+	public onRunComplete(contexts: Set<Context>, results: AggregatedResult) {
+		this.render({ status: 'runComplete', contexts, results })
+	}
+
+	public onTestCaseResult(test: Test, testCaseResult: AssertionResult) {
+		this.render({ status: 'testCaseResult', test, testCaseResult })
+	}
+
+	public onTestFileStart(test: Test) {
+		this.render({ status: 'testFileStart', test })
+	}
+
+	public getLastError() { }
+	public onTestResult(test: Test, testResult: TestResult) {
+		this.render({ status: 'testResult', test, testResult })
+	}
+
+	private render(obj: Record<string, any>) {
 		console.log(START_DIVIDER)
-		console.log(JSON.stringify({ status: 'testComplete', results: testResult }))
+		console.log(JSON.stringify(obj))
 		console.log(END_DIVIDER)
 	}
 
 	public onTestStart(test: Test) {
-		console.log(START_DIVIDER)
-		console.log(JSON.stringify({ status: 'testStart', test }))
-		console.log(END_DIVIDER)
+		this.render({ status: 'testStart', test })
 	}
 }
