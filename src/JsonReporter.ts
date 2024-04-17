c240d3f53396e326338972e654fddab35f2e8f50
Taylor Romero <me@taylorrome.ro>
2024-04-17 06:30:28 -0600

    patch: upgrade

--- src/JsonReporter.ts
+++ src/JsonReporter.ts
@@ -1,94 +1,98 @@
 import {
-	AggregatedResult,
-	TestContext,
-	Reporter,
-	Test,
-	TestResult,
+    AggregatedResult,
+    TestContext,
+    Reporter,
+    Test,
+    TestResult,
 } from '@jest/reporters'
 import { cycle } from './cycle'
 
 export declare type Milliseconds = number
 export declare type TestStatus =
-	| 'passed'
-	| 'failed'
-	| 'skipped'
-	| 'pending'
-	| 'todo'
-	| 'disabled'
-	| 'focused'
+    | 'passed'
+    | 'failed'
+    | 'skipped'
+    | 'pending'
+    | 'todo'
+    | 'disabled'
+    | 'focused'
 
-declare type CallSite = {
-	column: number
-	line: number
+declare interface CallSite {
+    column: number
+    line: number
 }
 
-export declare type AssertionResult = {
-	ancestorTitles: Array<string>
-	duration?: Milliseconds | null
-	failureDetails: Array<unknown>
-	failureMessages: Array<string>
-	fullName: string
-	invocations?: number
-	location?: CallSite | null
-	numPassingAsserts: number
-	status: TestStatus
-	title: string
+export declare interface AssertionResult {
+    ancestorTitles: string[]
+    duration?: Milliseconds | null
+    failureDetails: unknown[]
+    failureMessages: string[]
+    fullName: string
+    invocations?: number
+    location?: CallSite | null
+    numPassingAsserts: number
+    status: TestStatus
+    title: string
 }
 
 export const START_DIVIDER =
-	'***************************START_JSON_DIVIDER***************************'
+    '***************************START_JSON_DIVIDER***************************'
 
 export const END_DIVIDER =
-	'***************************END_JSON_DIVIDER***************************'
+    '***************************END_JSON_DIVIDER***************************'
 
 export default class JsonReporter implements Reporter {
-	public onTestFileResult(
-		test: Test,
-		testResult: TestResult,
-		aggregatedResult: AggregatedResult
-	) {
-		this.render({
-			status: 'onTestFileResult',
-			test,
-			testResult,
-			aggregatedResult,
-		})
-	}
+    public onTestFileResult(
+        test: Test,
+        testResult: TestResult,
+        aggregatedResult: AggregatedResult
+    ) {
+        this.render({
+            status: 'onTestFileResult',
+            test,
+            testResult,
+            aggregatedResult,
+        })
+    }
 
-	public onRunStart(results: AggregatedResult) {
-		this.render({ status: 'onRunStart', results })
-	}
+    public onRunStart(results: AggregatedResult) {
+        this.render({ status: 'onRunStart', results })
+    }
 
-	public onRunComplete(contexts: Set<TestContext>, results: AggregatedResult) {
-		this.render({ status: 'onRunComplete', contexts, results })
-	}
+    public onRunComplete(
+        contexts: Set<TestContext>,
+        results: AggregatedResult
+    ) {
+        this.render({ status: 'onRunComplete', contexts, results })
+    }
 
-	public onTestCaseResult(test: Test, testCaseResult: AssertionResult) {
-		this.render({ status: 'onTestCaseResult', test, testCaseResult })
-	}
+    public onTestCaseResult(test: Test, testCaseResult: AssertionResult) {
+        this.render({ status: 'onTestCaseResult', test, testCaseResult })
+    }
 
-	public onTestFileStart(test: Test) {
-		this.render({ status: 'onTestFileStart', test })
-	}
+    public onTestFileStart(test: Test) {
+        this.render({ status: 'onTestFileStart', test })
+    }
 
-	public getLastError() {
-		return undefined
-	}
+    public getLastError() {
+        return undefined
+    }
 
-	public onTestResult(test: Test, testResult: TestResult) {
-		this.render({ status: 'onTestResult', test, testResult })
-	}
+    public onTestResult(test: Test, testResult: TestResult) {
+        this.render({ status: 'onTestResult', test, testResult })
+    }
 
-	private render(obj: Record<string, any>) {
-		const string = cycle(obj)
-		const toWrite = START_DIVIDER + JSON.stringify(string) + END_DIVIDER + '\n'
-		console.log(toWrite)
-		return toWrite
-	}
+    private render(obj: Record<string, any>) {
+        const string = cycle(obj)
+        const toWrite =
+            START_DIVIDER + JSON.stringify(string) + END_DIVIDER + '\n'
+        console.log(toWrite)
+        return toWrite
+    }
 
-	public onTestStart(test: Test) {
-		this.render({ status: 'onTestStart', test })
-	}
+    public onTestStart(test: Test) {
+        this.render({ status: 'onTestStart', test })
+    }
 }
 
 export * from './cycle'
--- src/__tests__/Stringify.test.ts
+++ src/__tests__/Stringify.test.ts
@@ -3,35 +3,38 @@ import { retrocycle } from '../cycle'
 import JsonReporter, { END_DIVIDER, START_DIVIDER } from '../JsonReporter'
 
 export default class StringifyTest extends AbstractSpruceTest {
-	@test()
-	protected static canStringifyRecursive() {
-		let obj: Record<string, any> = {
-			go: 'team',
-			around: true,
-		}
+    @test()
+    protected static canStringifyRecursive() {
+        let obj: Record<string, any> = {
+            go: 'team',
+            around: true,
+        }
 
-		obj.recursive = obj
+        obj.recursive = obj
 
-		const reporter = new JsonReporter()
+        const reporter = new JsonReporter()
 
-		//@ts-ignore
-		const lastLogged = reporter.render(obj)
+        //@ts-ignore
+        const lastLogged = reporter.render(obj)
 
-		const parsed = JSON.parse(
-			//@ts-ignore
-			lastLogged.replace(START_DIVIDER, '').replace(END_DIVIDER, '').trim()
-		)
+        const parsed = JSON.parse(
+            //@ts-ignore
+            lastLogged
+                .replace(START_DIVIDER, '')
+                .replace(END_DIVIDER, '')
+                .trim()
+        )
 
-		assert.isEqualDeep(parsed, {
-			go: 'team',
-			around: true,
-			recursive: { $ref: '$' },
-		})
+        assert.isEqualDeep(parsed, {
+            go: 'team',
+            around: true,
+            recursive: { $ref: '$' },
+        })
 
-		const uncycled = retrocycle(parsed)
+        const uncycled = retrocycle(parsed)
 
-		assert.isEqual(uncycled.go, 'team')
-		assert.isEqual(uncycled.around, true)
-		assert.isTruthy(uncycled.recursive)
-	}
+        assert.isEqual(uncycled.go, 'team')
+        assert.isEqual(uncycled.around, true)
+        assert.isTruthy(uncycled.recursive)
+    }
 }
